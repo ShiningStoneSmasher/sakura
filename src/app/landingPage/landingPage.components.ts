dbe66df740b5de147582380e4e62490648f991af
ShiningStoneSmasher <klarlhauertstein@gmail.com>
2024-03-17 13:18:47 +0100

    testcommit

---
+++ src/app/landingPage/landingPage.components.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule],
+    template: '<h1>Hall Welt</h1>',
+    styleUrls: ['./landingPage.components.scss']
+})
+
+export class LandingPageComponents {}
